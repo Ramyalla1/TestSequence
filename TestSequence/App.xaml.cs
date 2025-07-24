022a688a92c7ef4cbc21e6874bab02ce0d475ae1
YALLA, RAM CHARAN <ram-charan.yalla.ext@siemens.com>
2025-07-24 17:12:36 +0530

    Add project files.

---
+++ TestSequence/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TestSequence
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ TestSequence/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSequence
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button clicked! You entered: " + myTextBox.Text);
+        }
+
+        private void myButton1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button clicked! You entered: " + myTextBox.Text);
+        }
+
+       
+    }
+}
